f279dfc8adf6b12b49ca9e66951e54384de86255
Jcacero <jcruz2013c@gmjail.com>
2021-09-17 14:27:26 -0300

    catalogo de anime

--- poo-ej3/anime.js
+++ poo-ej3/anime.js
@@ -12,11 +12,29 @@ export default class Anime {
 
        const datos_definitivos =  datos_convertidos.results
 
-       datos_definitivos.forEach(element => {
-            
-        console.log(element.image_url)
+       let array_catalogo= [];
 
+       datos_definitivos.forEach(element => {
+            let columna = `
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${element.image_url}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${element.title}</h5>
+                            <p class="card-text">${element.synopsis}</p>
+                            <p class="card-text"><small class="text-muted">${element.episodes}</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+            array_catalogo.push(columna)
        });
+
+       const catalogo = document.getElementById("catalogo").innerHTML=array_catalogo.join('')
         
     }
 }

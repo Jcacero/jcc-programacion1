e7274bfa6b566e1f87308927ce2357c98ed7d418
Jcacero <jcruz2013c@gmjail.com>
2021-09-23 16:54:54 -0300

    botones guardar y eliminar

--- firebase-ej1/main.js
+++ firebase-ej1/main.js
@@ -21,7 +21,53 @@ const guardarCliente = async () => {
     document.getElementById("inp_ape").value='';
     document.getElementById("inp_nom").value='';
     document.getElementById("inp_dni").value='';
+
+    listar_clientes();
 }
 
 const botono= document.getElementById("btn_obtener");
 botono.addEventListener("click",guardarCliente)
+
+//eliminar un cliente
+
+function eliminar(id){
+
+    db.collection("clientes").doc(id).delete()
+    listar_clientes();
+
+}
+
+const listar_clientes = async() => {
+    const clientes= await db.collection("clientes").get();
+    
+    let lista_clientes=[]    
+    
+    //odrdenamiento de los datos
+    const lista = clientes.docs.map(doc => {
+
+        lista_clientes = doc.data()
+        lista_clientes.id = doc.id;
+
+        return lista_clientes;
+
+    });
+
+    let total_filas = [];
+    lista.forEach(element => {
+        let fila= `
+            <tr>
+                <td>${element.nombre}</td>
+                <td>${element.apellido}</td>
+                <td>${element.dni}</td>
+                <td>
+                    <button onclick="eliminar('${element.id}')" class= "btn btn-danger btn-sm">X</button>
+                </td>            
+            </tr>
+        `
+        total_filas.push(fila)
+    });
+
+    document.getElementById("tbody").innerHTML = total_filas.join('')
+}
+//ejecutamos la funcion
+listar_clientes()
